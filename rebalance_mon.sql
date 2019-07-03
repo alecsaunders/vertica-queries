425e67c79874dc884e4e0eb5b8595ab289c12bb3
Alec Saunders <alecsaunders90@gmail.com>
2019-07-03 12:13:10 -0600

    adjusted tabs, don't track .DS_Store

--- rebalance_mon.sql
+++ rebalance_mon.sql
@@ -6,8 +6,8 @@ ORDER BY 2 DESC;
 SELECT *
 FROM system_sessions
 WHERE
-        session_type = 'REBALANCE_CLUSTER'
-        AND is_active
+  session_type = 'REBALANCE_CLUSTER'
+  AND is_active
 ;
 
 SELECT SUM(transferred_bytes), SUM(to_transfer_bytes), SUM(transferred_bytes) / (SUM(transferred_bytes) + SUM(to_transfer_bytes)), MAX(CURRENT_TIMESTAMP)
@@ -15,15 +15,15 @@ FROM rebalance_projection_status;
 
 SELECT rebalance_method Rebalance_method, Status, COUNT(*) AS Count
 FROM (
-        SELECT
-                rebalance_method,
-                CASE
-                        WHEN ( separated_percent = 100 AND transferred_percent = 100 ) THEN 'Completed'
-                        WHEN ( separated_percent <> 0 and separated_percent <> 100 ) OR ( transferred_percent <> 0 AND transferred_percent <> 100 ) THEN 'In Progress'
-                        ELSE 'Queued'
-                END AS  Status
-        FROM rebalance_projection_status
-        WHERE is_latest
+  SELECT
+    rebalance_method,
+    CASE
+      WHEN ( separated_percent = 100 AND transferred_percent = 100 ) THEN 'Completed'
+      WHEN ( separated_percent <> 0 and separated_percent <> 100 ) OR ( transferred_percent <> 0 AND transferred_percent <> 100 ) THEN 'In Progress'
+      ELSE 'Queued'
+    END AS  Status
+  FROM rebalance_projection_status
+  WHERE is_latest
 ) AS tab
 GROUP BY 1, 2
 ORDER BY 1, 2;
